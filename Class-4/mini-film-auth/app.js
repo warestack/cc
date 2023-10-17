42bbb0c1071c9b5b0fa621e7ecb1e4ff8505d755
Stelios Sotiriadis <steliosot@msn.com>
2023-10-17 19:06:19 +0100

    Update app.js

--- Class-4/mini-film-auth/app.js
+++ Class-4/mini-film-auth/app.js
@@ -13,9 +13,7 @@ const authRoute = require('./routes/auth')
 app.use('/api/film',filmsRoute)
 app.use('/api/user',authRoute)
 
-mongoose.connect(process.env.DB_CONNECTOR, ()=>{
-    console.log('DB is connected')
-})
+mongoose.connect(MURL).then(() => { console.log('Your mongoDB connector is on...')})
 
 app.listen(3000, ()=>{
     console.log('Server is running')
